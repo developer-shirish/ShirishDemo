e61051682c79b760e2d056ec68308242c8572a13
shirish <developer.shirish@gmail.com>
2016-10-04 21:32:53 +0530

    Second commit

--- app/src/main/java/com/example/shirish/shirishdemo/MainActivity.java
+++ app/src/main/java/com/example/shirish/shirishdemo/MainActivity.java
@@ -1,10 +1,9 @@
 package com.example.shirish.shirishdemo;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.TextView;
 
 import com.example.shirish.shirishdemo.image.upload.ImageUpload;
 import com.example.shirish.shirishdemo.open.camera.CameraActivity;
