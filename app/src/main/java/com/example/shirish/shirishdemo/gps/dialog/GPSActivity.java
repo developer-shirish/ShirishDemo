fc7a4624a78d55397b9479b251437d853850648c
shirish <developer.shirish@gmail.com>
2016-12-04 16:39:41 +0530

    Google DIalog

--- app/src/main/java/com/example/shirish/shirishdemo/gps/dialog/GPSActivity.java
+++ app/src/main/java/com/example/shirish/shirishdemo/gps/dialog/GPSActivity.java
@@ -8,10 +8,14 @@ import android.os.Bundle;
 import com.example.shirish.shirishdemo.R;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.PendingResult;
+import com.google.android.gms.common.api.ResultCallback;
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.LocationSettingsResult;
 
-public class GPSActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener{
+public class GPSActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, ResultCallback<LocationSettingsResult>{
 
     protected GoogleApiClient mGoogleAPIClient;
     protected LocationRequest mLocationRequest;
@@ -38,6 +42,12 @@ public class GPSActivity extends AppCompatActivity implements GoogleApiClient.Co
     @Override
     public void onConnected(@Nullable Bundle bundle) {
 
+        LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder().addLocationRequest(mLocationRequest);
+        builder.setAlwaysShow(true);
+
+        PendingResult<LocationSettingsResult> result = LocationServices.SettingsApi.checkLocationSettings(mGoogleAPIClient,  builder.build());
+
+        result.setResultCallback(this);
     }
 
     @Override
@@ -47,6 +57,11 @@ public class GPSActivity extends AppCompatActivity implements GoogleApiClient.Co
 
     @Override
     public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+
+    @Override
+    public void onResult(@NonNull LocationSettingsResult locationSettingsResult) {
 
     }
 }
